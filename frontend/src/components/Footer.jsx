df26e55dcb31343aae1d1c03f1b9072b54f56f56
Eduarda <euduardadomingues@gmail.com>
2025-01-26 21:09:06 -0300

    Feat: Rodapé.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../index.css";
+
+
+const Footer = () => {
+  return (
+    <footer className="footer-dark">
+      <div className="container">
+        <p>&copy; Desenvolvido por EduardaBeatrz.</p>
+      </div>
+    </footer>
+  );
+};
+
+
+export default Footer;
